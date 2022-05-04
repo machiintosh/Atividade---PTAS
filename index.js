dfd0569b41b414232747608fabb4402b7b1988aa
Machintosh <86083842+machiintosh@users.noreply.github.com>
2022-05-04 20:41:47 +0000

    Criado os métodos post e put

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ var { usuario } = require("./models")
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
-}))
+}));
 
-app.get("/", function(req, res){
-  var resultado = usuario.findaAll();
+app.get("/:id", async function(req, res){
+  const resultado = await usuario.findByPk(req.params.id);
   res.json(resultado);
 });
 
-app.post("/", function(req,res){
-  var resultado = usuario.create(req.body);
+app.get("/", async function(req, res){
+  const resultado = await usuario.findAll();
   res.json(resultado);
 });
 
+app.post("/", function(req, res){
+  const resultado = usuario.create(req.body);
+  res.json(resultado);
+});
+
+app.put("/:id", async function(req, res){
+  const atualizar = await usuario.findByPk(req.params.id);
+  res.send(atualizar.nome = "Igor")
+
+  const resultadoSave = await atualizar.save();
+  console.log(resultadoSave)
+})
+
+app.delete("/:id", async function(req, res){
+  const deletar = await usuario.findByPk(req.params.id);
+  deletar.destroy();
+})
 
 app.listen(3000, function(){
   console.log("O servidor está sambando com as araras")
